37e685944363fdeaecff142a8422cfe7400606ca
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-23 13:00:26 +0100

    anim

--- src/OpenGL/Bretzel.hs
+++ src/OpenGL/Bretzel.hs
@@ -1,14 +1,20 @@
 module OpenGL.Bretzel
   ( main )
   where
+import           Control.Concurrent             ( threadDelay )
+import           Control.Monad                  ( when )
+import qualified Data.ByteString               as B
 import           Data.IORef
-import           Data.Vector.Unboxed            ( (!)
-                                                , Vector
-                                                )
+import           Data.Vector.Unboxed            ( (!) )
+import           Graphics.Rendering.OpenGL.Capture
+                                                ( capturePPM )
 import           Graphics.Rendering.OpenGL.GL
                                          hiding ( Color )
 import           Graphics.UI.GLUT        hiding ( Color )
 import           MarchingCubes
+import           System.Directory               ( doesDirectoryExist )
+import           System.IO.Unsafe
+import           Text.Printf
 
 type F = Double
 type Triangles = [((XYZ F, XYZ F, XYZ F), (XYZ F, XYZ F, XYZ F))]
@@ -28,22 +34,23 @@ black = Color4 0 0 0 1
 red   = Color4 1 0 0 1
 
 fBretz :: XYZ F -> F
-fBretz (x,y,z) = ((x2 + y2/4 - 1) * (x2/4 + y2 - 1))^2 + z2
+fBretz (x,y,z) = sqr ((x2 + y2/4 - 1) * (x2/4 + y2 - 1)) + z2
   where
+  sqr u = u*u
   x2 = x*x
   y2 = y*y
   z2 = z*z
 
 voxel :: Voxel F
 voxel = makeVoxel fBretz ((-2.5, 2.5),(-2.5, 2.5),(-0.5, 0.5))
-                         (200, 200, 100)
+                         (300, 300, 150)
 
 trianglesIO :: IO Triangles
 trianglesIO = do 
-  msh <- makeMesh voxel 0.1
-  let vertices = _vertices msh
-      faces    = _faces msh
-      normals  = _normals msh
+  mesh <- makeMesh voxel 0.1
+  let vertices = _vertices mesh
+      faces    = _faces mesh
+      normals  = _normals mesh
       triangle face =
         ( (vertices ! i, vertices ! j, vertices ! k)
         , (normals ! i , normals ! j , normals ! k)
@@ -70,7 +77,7 @@ display context = do
   swapBuffers
   where
   drawTriangle ((v1, v2, v3), (n1, n2, n3)) = do
-      materialDiffuse FrontAndBack $= red
+      materialDiffuse Front $= red
       normal (toNormal n1)
       vertex (toVertex v1)
       normal (toNormal n2)
@@ -93,10 +100,16 @@ resize zoom s@(Size w h) = do
     w' = realToFrac w
     h' = realToFrac h
 
-keyboard :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
-         -> IORef Double -- zoom
-         -> KeyboardCallback
-keyboard rot1 rot2 rot3 zoom c _ = do
+keyboard
+  :: IORef GLfloat
+  -> IORef GLfloat
+  -> IORef GLfloat -- rotations
+  -> IORef Double  -- zoom
+  -> IORef Bool    -- animation
+  -> IORef Int     -- animation delay
+  -> IORef Bool    -- save animation
+  -> KeyboardCallback
+keyboard rot1 rot2 rot3 zoom anim delay save c _ = do
   case c of
     'e' -> rot1 $~! subtract 2
     'r' -> rot1 $~! (+ 2)
@@ -104,11 +117,41 @@ keyboard rot1 rot2 rot3 zoom c _ = do
     'y' -> rot2 $~! (+ 2)
     'u' -> rot3 $~! subtract 2
     'i' -> rot3 $~! (+ 2)
-    'm' -> zoom $~! (+ 1)
-    'l' -> zoom $~! subtract 1
+    'm' -> zoom $~! (+ 0.25)
+    'l' -> zoom $~! subtract 0.25
+    'a' -> anim $~! not
+    'o' -> delay $~! (+ 10000)
+    'p' -> delay $~! (\d -> if d == 0 then 0 else d - 10000)
+    's' -> save $~! not
     'q' -> leaveMainLoop
     _   -> return ()
   postRedisplay Nothing
+
+ppmExists :: Bool
+{-# NOINLINE ppmExists #-}
+ppmExists = unsafePerformIO $ doesDirectoryExist "./ppm"
+
+idle
+  :: IORef Bool
+  -> IORef Int
+  -> IORef Bool
+  -> IORef Int
+  -> IORef GLfloat
+  -> IdleCallback
+idle anim delay save snapshots rot3 = do
+  a        <- get anim
+  snapshot <- get snapshots
+  s        <- get save
+  when a $ do
+    d <- get delay
+    when (s && ppmExists && snapshot < 180) $ do
+      let ppm = printf "ppm/pic%04d.ppm" snapshot
+      (>>=) capturePPM (B.writeFile ppm)
+      print snapshot
+      snapshots $~! (+ 1)
+    rot3 $~! (+ 2)
+    _ <- threadDelay d
+    postRedisplay Nothing
 
 
 main :: IO ()
@@ -118,7 +161,7 @@ main = do
   windowSize $= Size 512 512
   initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
   clearColor $= white
-  materialAmbient FrontAndBack $= black
+  materialAmbient Front $= black
   lighting $= Enabled
   lightModelTwoSide $= Enabled
   light (Light 0) $= Enabled
@@ -128,6 +171,7 @@ main = do
   specular (Light 0) $= white
   depthFunc $= Just Less
   shadeModel $= Smooth
+  cullFace $= Just Back
   rot1 <- newIORef 0.0
   rot2 <- newIORef 0.0
   rot3 <- newIORef 0.0
@@ -140,13 +184,20 @@ main = do
                                       contextZoom = zoom,
                                       contextTriangles = triangles}
   reshapeCallback $= Just (resize 0)
-  keyboardCallback $= Just (keyboard rot1 rot2 rot3 zoom)
-  idleCallback $= Nothing
+  anim      <- newIORef False
+  delay     <- newIORef 0
+  save      <- newIORef False
+  snapshots <- newIORef 0
+  keyboardCallback $= Just (keyboard rot1 rot2 rot3 zoom anim delay save)
+  idleCallback $= Just (idle anim delay save snapshots rot3)
   putStrLn "*** Bretzel ***\n\
         \    To quit, press q.\n\
         \    Scene rotation:\n\
         \        e, r, t, y, u, i\n\
         \    Zoom: l, m\n\
+        \    Animation: a\n\
+        \    Animation speed: o, p\n\
+        \    Save animation: s\n\
         \"
   mainLoop
 
